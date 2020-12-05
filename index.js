f809adb45b49a3fdf5e188c4cefb2ef51e76472e
FaresEsprit20 <63284587+FaresEsprit20@users.noreply.github.com>
2020-12-05 21:50:40 +0000

    first commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var con = mysql.createConnection({
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(express.static('images'));
 
 //connect to database
 con.connect((err) =>{
